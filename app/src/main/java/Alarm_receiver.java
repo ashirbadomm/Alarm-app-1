52b206c9637d7a961a9bd79a9e005dd64e0fa57b
ASHIRBAD SAHU <33455630+ashirbadomm@users.noreply.github.com>
2017-11-08 07:13:17 +0530

    Initial commit

---
+++ app/src/main/java/Alarm_receiver.java
@@ -0,0 +1,7 @@
+/**
+ * Created by DELL on 10/13/2017.
+ */
+
+public class Alarm_receiver {
+
+}
---
+++ app/src/main/java/ash/example/dell/alarm/RingtonePlayingService.java
@@ -0,0 +1,10 @@
+package ash.example.dell.alarm;
+
+import android.app.Service;
+
+/**
+ * Created by DELL on 10/13/2017.
+ */
+
+public class RingtonePlayingService extends Service {
+}
